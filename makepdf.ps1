893177d4e86e0754a09050027eea1c747bb65acd
saba161 <saba.koguashvili1999@mail.ru>
2020-02-23 15:13:55 +0400

    resume

---
+++ makepdf.ps1
@@ -0,0 +1,26 @@
+$file = "README"
+$fileName = "$file.md"
+$uri = "http://www.markdowntopdf.com/app/upload"
+$outputFileName = "Saba Koghuashvili CV.pdf"
+$currentPath = Convert-Path .
+$filePath="$currentPath\$fileName"
+
+$fileBin = [System.IO.File]::ReadAlltext($filePath)
+$boundary = [System.Guid]::NewGuid().ToString()
+$LF = "`r`n"
+$bodyLines = (
+    "--$boundary",
+    "Content-Disposition: form-data; name=`"file`"; filename=`"$fileName`"",
+    "Content-Type: application/octet-stream$LF",
+    $fileBin,
+    "--$boundary--$LF"
+) -join $LF
+
+$response = Invoke-RestMethod -Uri $uri -Method Post -ContentType "multipart/form-data; boundary=`"$boundary`"" -Body $bodyLines
+echo $response
+
+Start-Sleep -s 1
+
+$url = "http://www.markdowntopdf.com/app/download/$($response.foldername)/$file"
+echo $url
+Invoke-WebRequest -Uri $url -OutFile $outputFileName
